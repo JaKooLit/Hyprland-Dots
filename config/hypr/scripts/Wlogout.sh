68fe5e3461d9e5d17e352708b1c10deb44a0f922
JaKooLit <jimmilovejay@gmail.com>
2023-11-04 08:27:54 +0900

    improved wlogout script. added timeout of 30 secs

--- config/hypr/scripts/Wlogout.sh
+++ config/hypr/scripts/Wlogout.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-wlogout --protocol layer-shell -b 5 -T 600 -B 600 && disown
- 
+# Start wlogout in the background
+wlogout --protocol layer-shell -b 5 -T 600 -B 600 &
+
+# Capture the PID of the wlogout process
+wlogout_pid=$!
+
+# Wait for up to 30 seconds for wlogout to exit gracefully
+timeout 30s tail --pid $wlogout_pid -f /dev/null
+
+# If wlogout is still running after the timeout, forcefully kill it
+if ps -p $wlogout_pid > /dev/null; then
+  kill -KILL $wlogout_pid
+fi
