8f2b0a6d7437e73f777cfad809fb950c0f2c9491
JaKooLit <jimmielovejay@gmail.com>
2023-11-06 10:34:28 +0900

    initial boot script to for pywal & wallpaper set

---
+++ config/hypr/initial-boot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# THIS SCRIPT CAN BE DELETED ONCE BOOTED!!
+
+# A bash script designed to ran it only once dotfiles installed
+# Check if a marker file exists.
+if [ ! -f ~/.hyprland_startup_done ]; then
+
+	# Check if the ~/.cache/wal directory exists
+	if [ ! -d ~/.cache/wal ]; then
+    	printf " Initializing pywal........\n\n"
+    	# Check if the ~/Pictures/wallpapers directory exists
+    	if [ -d ~/Pictures/wallpapers ]; then
+        	# Run wal with random wallpapers from ~/Pictures/wallpapers
+        	wal -i ~/Pictures/wallpapers/*
+        	echo "Pywal initialized"
+		fi
+	fi
+
+	# Initializing the initial wallpaper and wal
+	exec ~/.config/hypr/scripts/Wallpaper.sh 
+
+    # Create a marker file to indicate that the script has been executed.
+    touch ~/.hyprland_startup_done
+fi
+
+
