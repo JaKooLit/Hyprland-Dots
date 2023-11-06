c6c01a8accf7a28a2c837cb7937f8f5f805433ff
JaKooLit <jimmielovejay@gmail.com>
2023-11-06 22:30:46 +0900

    small tweak

--- config/hypr/scripts/ClipManager.sh
+++ config/hypr/scripts/ClipManager.sh
@@ -3,8 +3,7 @@
 # Clipboard Manager
 
 if [[ ! $(pidof rofi) ]]; then
-	#cliphist list | rofi -dmenu -config ~/.config/rofi/config-cliphist.rasi | cliphist decode | wl-copy
-	cliphist list | rofi -dmenu | cliphist decode | wl-copy
+	cliphist list | rofi -dmenu -config ~/.config/rofi/config-long.rasi | cliphist decode | wl-copy
 else
 	pkill rofi
 fi
