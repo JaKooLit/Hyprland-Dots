538f6ec00698215204288ca91c91c736792b38cf
JaKooLit <jimmielovejay@gmail.com>
2023-11-13 17:56:21 +0900

    added polkit-gnome arch

--- config/hypr/scripts/Polkit.sh
+++ config/hypr/scripts/Polkit.sh
@@ -2,6 +2,7 @@
 
 # Polkit possible paths files to check
 polkit=(
+  "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
   "/usr/lib/polkit-kde-authentication-agent-1"
   "/usr/lib/polkit-gnome-authentication-agent-1"
   "/usr/libexec/polkit-gnome-authentication-agent-1"
