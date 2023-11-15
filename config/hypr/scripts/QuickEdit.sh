13dd55b3147a0a62091dfa412d13d7f4d0ea2646
JaKooLit <jimmielovejay@gmail.com>
2023-11-15 10:22:09 +0900

    added laptop-keybinds on Quick Edit menu (SUPER E)

--- config/hypr/scripts/QuickEdit.sh
+++ config/hypr/scripts/QuickEdit.sh
@@ -5,10 +5,11 @@ hyprDir="$HOME/.config/hypr/configs"
 menu(){
   printf "1. view Env-variables\n"
   printf "2. view Rules\n"
-  printf "3. view Execs\n"
-  printf "4. view Key-Binds\n"
+  printf "3. view Execs (startup)\n"
+  printf "4. view KeyBinds\n"
   printf "5. view Monitors\n"
-  printf "6. view Hyprland-Settings\n"
+  printf "6. view Laptop-Keybinds\n"
+  printf "7. view Hyprland-Settings\n"
 }
 
 main() {
@@ -30,6 +31,9 @@ main() {
             kitty -e nano "$hyprDir/Monitors.conf"
             ;;
         6)
+            kitty -e nano "$hyprDir/Laptops.conf"
+            ;;
+        7)
             kitty -e nano "$hyprDir/Settings.conf"
             ;;
         *)
