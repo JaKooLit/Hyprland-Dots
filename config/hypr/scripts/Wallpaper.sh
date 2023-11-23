ab573dec63693a438da1e93d951750bcfb4e037d
JaKooLit <jimmielovejay@gmail.com>
2023-11-24 01:29:06 +0900

    clean up wallpaper Script

--- config/hypr/scripts/Wallpaper.sh
+++ config/hypr/scripts/Wallpaper.sh
@@ -4,48 +4,8 @@ DIR=$HOME/Pictures/wallpapers/
 PICS=($(find ${DIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" \)))
 RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
 
-change_swaybg(){
-  pkill swww
-  pkill swaybg
-  swaybg -m fill -i ${RANDOMPICS}
-}
-
-change_swww(){
-  pkill swaybg
-  swww query || swww init
-  swww img ${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
-}
-
-change_current(){
-  if pidof swaybg >/dev/null; then
-    change_swaybg
-  else
-    change_swww
-  fi
-}
-
-switch(){
-  if pidof swaybg >/dev/null; then
-    change_swww
-  else
-    change_swaybg
-  fi
-}
-
-case "$1" in
-	"swaybg")
-		change_swaybg
-		;;
-	"swww")
-		change_swww
-		;;
-  "s")
-		switch
-		;;
-	*)
-		change_current
-		;;
-esac
+swww query || swww init
+swww img ${RANDOMPICS} --transition-fps 30 --transition-type any --transition-duration 3
 
 exec $HOME/.config/hypr/scripts/PywalSwww.sh &
 sleep 1
