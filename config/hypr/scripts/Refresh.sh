9f7677f89b34aa8fe0e61aa4c84ae556b66dd05a
JaKooLit <jimmielovejay@gmail.com>
2023-11-30 01:58:25 +0900

    Restart of Rainbow Borders fixed :)

--- config/hypr/scripts/Refresh.sh
+++ config/hypr/scripts/Refresh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPTSDIR=$HOME/.config/hypr/scripts
 
@@ -6,7 +6,7 @@ SCRIPTSDIR=$HOME/.config/hypr/scripts
 _ps=(waybar rofi)
 for _prs in "${_ps[@]}"; do
     if pidof "${_prs}" >/dev/null; then
-        killall "${_prs}"
+        pkill "${_prs}"
     fi
 done
 
@@ -16,6 +16,6 @@ waybar &
 # Relaunch dunst with pywal-borders
 ${SCRIPTSDIR}/PywalDunst.sh &
 
-## trying to figure out how to restart Rainbow borders
-#sleep 1
-#${SCRIPTSDIR}/RainbowBorders.sh &
+# Relaunching rainbow borders
+sleep 1
+${SCRIPTSDIR}/RainbowBorders.sh &
