bee281ef322bb05157cd810faa509cfe90e379c3
SherLock707 <kirangeorge1995@gmail.com>
2023-12-29 14:05:05 +0530

    Added notification tone for swaync notification

--- config/hypr/scripts/Brightness.sh
+++ config/hypr/scripts/Brightness.sh
@@ -26,7 +26,7 @@ get_icon() {
 
 # Notify
 notify_user() {
-	notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Brightness : $current%"
+	notify-send -e -a brightness -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Brightness : $current%"
 }
 
 # Change brightness
--- config/hypr/scripts/BrightnessKbd.sh
+++ config/hypr/scripts/BrightnessKbd.sh
@@ -24,7 +24,7 @@ get_icon() {
 }
 # Notify
 notify_user() {
-	notify-send -e -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Keyboard Brightness : $current%"
+	notify-send -e -a brightness -h string:x-canonical-private-synchronous:brightness_notif -h int:value:$current -u low -i "$icon" "Keyboard Brightness : $current%"
 }
 
 # Change brightness
