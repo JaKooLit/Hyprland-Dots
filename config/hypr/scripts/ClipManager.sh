5c56957b8728d6b5fc513b331d8f1fb7f06f8688
PostCyberPunk <lyhguest2@outlook.com>
2024-01-07 20:07:07 +0800

    feat(clipboard):Control+Delete to remove entry

--- config/hypr/scripts/ClipManager.sh
+++ config/hypr/scripts/ClipManager.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
-## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Clipboard Manager. This needed cliphist & wl-copy and of course rofi
-
-if [[ ! $(pidof rofi) ]]; then
-	cliphist list | rofi -dmenu -config ~/.config/rofi/config-long.rasi | cliphist decode | wl-copy
-else
-	pkill rofi
-fi
+#!/usr/bin/env bash
+#
+while [[ true ]]; do
+	result=$(
+		cliphist list | rofi -dmenu \
+			-kb-custom-1 "Control-Delete" \
+			-config ~/.config/rofi/config-long.rasi
+	)
+	exit_state=$?
+	if [[ $exit_state -eq 1 ]]; then
+		exit
+	fi
+	case "$exit_state" in
+	0)
+		cliphist decode <<<$result | wl-copy
+		exit
+		;;
+	10)
+		cliphist delete <<<$result
+		;;
+	esac
+done
