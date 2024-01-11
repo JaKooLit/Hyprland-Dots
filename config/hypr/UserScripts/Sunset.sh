75e2b0360ba5ab9735711469351029a414950ec4
Ja.KooLit <ejhay.games@gmail.com>
2024-01-11 22:55:03 +0900

    dropped wlsunset

--- config/hypr/UserScripts/Sunset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-
-# wlsunset project page
-# https://sr.ht/~kennylevinsen/wlsunset/
-
-wlsunset -t 4000 -T 6500 -d 900 -S 07:00 -s 19:00
