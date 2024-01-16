82baf3e31dce375f32678117df1522f4cf9756fc
Ja.KooLit <ejhay.games@gmail.com>
2024-01-17 05:39:46 +0900

    Update ClipManager.sh

--- config/hypr/scripts/ClipManager.sh
+++ config/hypr/scripts/ClipManager.sh
@@ -1,22 +1,40 @@
-#!/usr/bin/env bash
-#
-while [[ true ]]; do
-	result=$(
-		cliphist list | rofi -dmenu \
-			-kb-custom-1 "Control-Delete" \
-			-config ~/.config/rofi/config-long.rasi
-	)
-	exit_state=$?
-	if [[ $exit_state -eq 1 ]]; then
-		exit
-	fi
-	case "$exit_state" in
-	0)
-		cliphist decode <<<$result | wl-copy
-		exit
-		;;
-	10)
-		cliphist delete <<<$result
-		;;
-	esac
+#!/bin/bash
+## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Clipboard Manager. This script uses cliphist, rofi, and wl-copy.
+
+# Actions:
+# CTRL Del to delete an entry
+# ALT Del to wipe clipboard contents
+
+while true; do
+    result=$(
+        rofi -dmenu \
+            -kb-custom-1 "Control-Delete" \
+            -kb-custom-2 "Alt-Delete" \
+            -config ~/.config/rofi/config-clipboard.rasi < <(cliphist list)
+    )
+
+    case "$?" in
+        1)
+            exit
+            ;;
+        0)
+            case "$result" in
+                "")
+                    continue
+                    ;;
+                *)
+                    cliphist decode <<<"$result" | wl-copy
+                    exit
+                    ;;
+            esac
+            ;;
+        10)
+            cliphist delete <<<"$result"
+            ;;
+        11)
+            cliphist wipe
+            ;;
+    esac
 done
+
