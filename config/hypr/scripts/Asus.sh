e299cf697bdff6ed8676159ccf4ab794b0441a5f
KKV9 <ciaranob.cob@gmail.com>
2024-02-03 23:22:13 +0000

    Revert "Notify chaning fan profiles on asus laptops"
    This reverts commit ba91977fe84d1858d41ee5eb644ca526bda19738.

--- config/hypr/scripts/Asus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Changing fan profiles on Asus laptops
-
-notif="$HOME/.config/swaync/images/bell.png"
-
-asusctl profile -n
-msg=$(asusctl profile -p)
-notify-send -u low -i "$notif" "$msg"
