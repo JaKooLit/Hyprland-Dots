1144a237ec99a2e26380065623d180f9db450218
Kiran George <kirangeorge1995@gmail.com>
2024-03-24 13:15:35 +0530

    Added Hypridle and Hyprlock

--- config/hypr/scripts/LockScreen.sh
+++ config/hypr/scripts/LockScreen.sh
@@ -2,6 +2,10 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # For Swaylock
 
-CONFIG="$HOME/.config/swaylock/config"
+# CONFIG="$HOME/.config/swaylock/config"
 
-sleep 0.5s; swaylock --config ${CONFIG} & disown
+# sleep 0.5s; swaylock --config ${CONFIG} & disown
+
+
+# For Hyprlock
+hyprlock -q
