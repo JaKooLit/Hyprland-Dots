d1b151fe3778b38b53fb9288b024b37fdccb6c27
Ja.KooLit <ejhay.games@gmail.com>
2024-04-20 00:01:45 +0900

    updated volume.sh

--- config/hypr/scripts/Volume.sh
+++ config/hypr/scripts/Volume.sh
@@ -44,7 +44,7 @@ inc_volume() {
     if [ "$(pamixer --get-mute)" == "true" ]; then
         toggle_mute
     else
-        pamixer -i 5 && notify_user
+        pamixer -i 5 --allow-boost --set-limit 150 && notify_user
     fi
 }
 
