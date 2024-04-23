73a945572bf3200568834c4efe358494d507de15
JaKooLit <jimmielovejay@gmail.com>
2024-04-23 09:47:54 +0900

    Bump to v2.2.8 Featuring Hypridle and Hyprlock. Thanks to @SherLock707 for the configs

--- config/hypr/scripts/LockScreen.sh
+++ config/hypr/scripts/LockScreen.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# For Swaylock
 
-CONFIG="$HOME/.config/swaylock/config"
-
-sleep 0.5s; swaylock --config ${CONFIG} & disown
+# For Hyprlock
+hyprlock -q
