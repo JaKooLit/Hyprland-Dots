341298c4d2441bda3a5390f4fd4b2586ef9edb8a
Kiran George <kirangeorge1995@gmail.com>
2024-05-18 07:48:44 +0530

    Added Waypaper as wallpaper switcher if you have it installed

---
+++ config/hypr/UserScripts/WallpaperSelectWaypaper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# This script for setting colours for wallaper selected by Waypaper(SUPER W)
+
+SCRIPTSDIR="$HOME/.config/hypr/scripts"
+
+sleep 0.5
+${SCRIPTSDIR}/WallustSwww.sh
+sleep 0.2
+${SCRIPTSDIR}/Refresh.sh
