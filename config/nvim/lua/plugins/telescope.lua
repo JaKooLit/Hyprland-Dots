fd0344c5e578cbd6b1a4a5dff18e927956157e02
FernuDev <fernu292@gmail.com>
2024-07-07 19:07:51 -0600

    feat: add neovim configuration files

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,16 +8,6 @@ return {
         end
     },
     {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown ({})
-                    }
-                }
-            })
-            require("telescope").load_extensions("ui-select")
-        end
+        "nvim-telescope/telescope-ui-select.nvim"
     }
 }
