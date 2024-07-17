877f1a9fd50ec27c6c513863dca9be618a910769
Ezejim Amblessed <amblessedezejim@gmail.com>
2024-07-17 03:34:04 +0100

    fix: Enable hyprland animations by toggling gamemode off

--- config/hypr/scripts/GameMode.sh
+++ config/hypr/scripts/GameMode.sh
@@ -5,10 +5,9 @@
 notif="$HOME/.config/swaync/images/bell.png"
 SCRIPTSDIR="$HOME/.config/hypr/scripts"
 
-
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ] ; then
-    hyprctl --batch "\
+if [ "$HYPRGAMEMODE" = 1 ]; then
+  hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:passes 0;\
@@ -16,16 +15,15 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-    swww kill 
-    notify-send -e -u low -i "$notif" "gamemode enabled. All animations off"
-    exit
+  swww kill
+  notify-send -e -u low -i "$notif" "gamemode enabled. All animations off"
+  exit
 else
-	swww-daemon --format xrgb && swww img "$HOME/.config/rofi/.current_wallpaper" &
-	sleep 0.1
-	${SCRIPTSDIR}/WallustSwww.sh
-	sleep 0.5
-	${SCRIPTSDIR}/Refresh.sh	 
-    notify-send -e -u normal -i "$notif" "gamemode disabled. All animations normal"
-    exit
+  swww-daemon --format xrgb && swww img "$HOME/.config/rofi/.current_wallpaper" &
+  sleep 0.1
+  ${SCRIPTSDIR}/WallustSwww.sh
+  sleep 0.5
+  ${SCRIPTSDIR}/Refresh.sh
+  notify-send -e -u normal -i "$notif" "gamemode disabled. All animations normal"
+  hyprctl reload
 fi
-hyprctl reload
