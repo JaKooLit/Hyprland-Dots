f8d092d21098ab872cbb2bd3316d35e299861f7f
JaKooLit <jimmielovejay@gmail.com>
2024-08-23 01:17:23 +0900

    updated initial waybar style

--- config/hypr/initial-boot.sh
+++ config/hypr/initial-boot.sh
@@ -8,7 +8,7 @@
 # Variables
 scriptsDir=$HOME/.config/hypr/scripts
 wallpaper=$HOME/.config/hypr/wallpaper_effects/.wallpaper_modified
-waybar_style="$HOME/.config/waybar/style/[Dark] Half-Moon.css"
+waybar_style="$HOME/.config/waybar/style/[Colored] Chroma Glow.css"
 kvantum_theme="Catppuccin-Mocha"
 color_scheme="prefer-dark"
 gtk_theme="Andromeda-dark"
