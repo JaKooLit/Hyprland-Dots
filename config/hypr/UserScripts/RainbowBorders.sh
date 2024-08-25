159f943a70c20c3e8ca7a7be2154dd8e05555874
JaKooLit <ejhay.games@gmail.com~>
2024-08-25 13:56:58 +0900

    a Pre-release

--- config/hypr/UserScripts/RainbowBorders.sh
+++ config/hypr/UserScripts/RainbowBorders.sh
@@ -5,6 +5,8 @@ function random_hex() {
     echo $random_hex
 }
 
+# rainbow colors only for active window
 hyprctl keyword general:col.active_border $(random_hex)  $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex)  270deg
 
-hyprctl keyword general:col.inactive_border $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex)  270deg
+# rainbow colors for inactive window (uncomment to take effect)
+#hyprctl keyword general:col.inactive_border $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) 270deg
