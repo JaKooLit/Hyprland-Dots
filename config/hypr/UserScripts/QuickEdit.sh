ca7d25bfb4174e11da3def6a42cf828a704ef85d
JaKooLit <jimmielovejay@gmail.com>
2024-09-17 00:21:34 +0900

    simplified quickedit; some formatting tweak

--- config/hypr/UserScripts/QuickEdit.sh
+++ config/hypr/UserScripts/QuickEdit.sh
@@ -1,66 +1,53 @@
 #!/bin/bash
-# Rofi menu for Quick Edit / View of Settings (SUPER E)
+# Rofi menu for Quick Edit/View of Settings (SUPER E)
 
-# define your preferred text editor and terminal to use
-editor=${EDITOR:-nano}
+# Define preferred text editor and terminal
+edit=${EDITOR:-nano}
 tty=kitty
 
+# Paths to configuration directories
 configs="$HOME/.config/hypr/configs"
 UserConfigs="$HOME/.config/hypr/UserConfigs"
 
-menu(){
-  printf "1. edit Env-variables\n"
-  printf "2. edit Window-Rules\n"
-  printf "3. edit Startup_Apps\n"
-  printf "4. edit User-Keybinds\n"
-  printf "5. edit Monitors\n"
-  printf "6. edit Laptop-Keybinds\n"
-  printf "7. edit User-Settings\n"
-  printf "8. edit Decorations & Animations\n"
-  printf "9. edit Workspace-Rules\n"
-  printf "10. edit Default-Settings\n"
-  printf "11. edit Default-Keybinds\n"
+# Function to display the menu options
+menu() {
+    cat <<EOF
+1. Edit Env-variables
+2. Edit Window-Rules
+3. Edit Startup_Apps
+4. Edit User-Keybinds
+5. Edit Monitors
+6. Edit Laptop-Keybinds
+7. Edit User-Settings
+8. Edit Decorations & Animations
+9. Edit Workspace-Rules
+10. Edit Default-Settings
+11. Edit Default-Keybinds
+EOF
 }
 
+# Main function to handle menu selection
 main() {
     choice=$(menu | rofi -i -dmenu -config ~/.config/rofi/config-compact.rasi | cut -d. -f1)
+    
+    # Map choices to corresponding files
     case $choice in
-        1)
-            $tty $editor "$UserConfigs/ENVariables.conf"
-            ;;
-        2)
-            $tty $editor "$UserConfigs/WindowRules.conf"
-            ;;
-        3)
-            $tty $editor "$UserConfigs/Startup_Apps.conf"
-            ;;
-        4)
-            $tty $editor "$UserConfigs/UserKeybinds.conf"
-            ;;
-        5)
-            $tty $editor "$UserConfigs/Monitors.conf"
-            ;;
-        6)
-            $tty $editor "$UserConfigs/Laptops.conf"
-            ;;
-        7)
-            $tty $editor "$UserConfigs/UserSettings.conf"
-            ;;
-        8)
-            $tty $editor "$UserConfigs/UserDecorAnimations.conf"
-            ;;
-        9)
-            $tty $editor "$UserConfigs/WorkspaceRules.conf"
-            ;;            
-		10)
-            $tty $editor "$configs/Settings.conf"
-            ;;
-        11)
-            $tty $editor "$configs/Keybinds.conf"
-            ;;
-        *)
-            ;;
+        1) file="$UserConfigs/ENVariables.conf" ;;
+        2) file="$UserConfigs/WindowRules.conf" ;;
+        3) file="$UserConfigs/Startup_Apps.conf" ;;
+        4) file="$UserConfigs/UserKeybinds.conf" ;;
+        5) file="$UserConfigs/Monitors.conf" ;;
+        6) file="$UserConfigs/Laptops.conf" ;;
+        7) file="$UserConfigs/UserSettings.conf" ;;
+        8) file="$UserConfigs/UserDecorAnimations.conf" ;;
+        9) file="$UserConfigs/WorkspaceRules.conf" ;;
+        10) file="$configs/Settings.conf" ;;
+        11) file="$configs/Keybinds.conf" ;;
+        *) return ;;  # Do nothing for invalid choices
     esac
+
+    # Open the selected file in the terminal with the text editor
+    $tty -e $edit "$file"
 }
 
 main
