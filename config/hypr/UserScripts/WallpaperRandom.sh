239e30d295bf7a58e2ac00a79d959704b3ba8fe1
JaKooLit <ejhay.games@gmail.com>
2025-02-19 13:27:11 +0900

    added a function to check first if kitty installed. This is a idiot notification who uninstall kitty and complain that script wont work

--- config/hypr/UserScripts/WallpaperRandom.sh
+++ config/hypr/UserScripts/WallpaperRandom.sh
@@ -5,7 +5,7 @@
 wallDIR="$HOME/Pictures/wallpapers"
 SCRIPTSDIR="$HOME/.config/hypr/scripts"
 
-focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
+focused_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
 
 PICS=($(find -L ${wallDIR} -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.pnm" -o -name "*.tga" -o -name "*.tiff" -o -name "*.webp" -o -name "*.bmp" -o -name "*.farbfeld" -o -name "*.gif" \)))
 RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
