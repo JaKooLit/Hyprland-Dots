f3f55304bd8e7949915cd8a1d8000509bbeea186
JaKooLit <ejhay.games@gmail.com>
2025-03-12 01:36:12 +0900

    ok kinda like revert

--- config/hypr/scripts/KeyHints.sh
+++ config/hypr/scripts/KeyHints.sh
@@ -28,7 +28,7 @@ GDK_BACKEND=$BACKEND yad \
 " SHIFT E" "KooL Hyprland Settings Menu" "" \
 "" "" "" \
 " enter" "Terminal" "(kitty)" \
-" SHIFT enter" "DropDown Terminal" " Q to close" \
+" SHIFT enter" "DropDown Terminal" "(Not available in Debian and Ubuntu)" \
 " B" "Launch Browser" "(Default browser)" \
 " A" "Desktop Overview" "(AGS - if opted to install)" \
 " D" "Application Launcher" "(rofi-wayland)" \
@@ -36,7 +36,7 @@ GDK_BACKEND=$BACKEND yad \
 " S" "Google Search using rofi" "(rofi)" \
 " Q" "close active window" "(not kill)" \
 " Shift Q " "kills an active window" "(kill)" \
-" ALT mouse scroll up/down   " "Desktop Zoom" "Desktop Magnifier" \
+" Z" "Desktop Zoom" "(Not available in Debian and Ubuntu)" \
 " Alt V" "Clipboard Manager" "(cliphist)" \
 " W" "Choose wallpaper" "(Wallpaper Menu)" \
 " Shift W" "Choose wallpaper effects" "(imagemagick + swww)" \
