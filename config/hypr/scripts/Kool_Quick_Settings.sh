dffd296b92f06b1606f8d03137fbf235a39d261b
JaKooLit <ejhay.games@gmail.com>
2025-03-20 15:33:11 +0900

    Added Kitty Theme Selector - Added in KooL hyprland Settings rofi. Kool Settings rofi modified since it is basically expanding

--- config/hypr/scripts/Kool_Quick_Settings.sh
+++ config/hypr/scripts/Kool_Quick_Settings.sh
@@ -32,6 +32,7 @@ view/edit Decorations
 view/edit Animations
 view/edit Laptop Keybinds
 view/edit Default Keybinds
+Choose Kitty Terminal Theme
 Configure Monitors (nwg-displays)
 Configure Workspace Rules (nwg-displays)
 GTK Settings (nwg-look)
@@ -62,6 +63,7 @@ main() {
         "view/edit Animations") file="$UserConfigs/UserAnimations.conf" ;;
         "view/edit Laptop Keybinds") file="$UserConfigs/Laptops.conf" ;;
         "view/edit Default Keybinds") file="$configs/Keybinds.conf" ;;
+        "Choose Kitty Terminal Theme") $scriptsDir/Kitty_themes.sh ;;
         "Configure Monitors (nwg-displays)") 
             if ! command -v nwg-displays &>/dev/null; then
                 notify-send -i "$iDIR/ja.png" "E-R-R-O-R" "Install nwg-displays first"
