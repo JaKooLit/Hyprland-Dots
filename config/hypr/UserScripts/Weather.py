7903b59ce5fc28c6f27bdecbce817c7de75f44fb
Daniel Walker <haxxordan@proton.me>
2025-04-30 14:35:45 -0700

    fetch wind_speed correctly for current site layout

--- config/hypr/UserScripts/Weather.py
+++ config/hypr/UserScripts/Weather.py
@@ -88,7 +88,7 @@ temp_max = (
 temp_min_max = f"  {temp_min}\t\t  {temp_max}"
 
 # wind speed
-wind_speed = html_data("span[data-testid='Wind']").text().split("\n")[0]
+wind_speed = html_data("span[data-testid='Wind'] > span").eq(1)[0].text
 wind_text = f"  {wind_speed}"
 
 # humidity
