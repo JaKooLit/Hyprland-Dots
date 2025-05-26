c6c06f0448413454b9ea58af7526c55c360b0e97
JaKooLit <ejhay.games@gmail.com>
2025-05-27 03:58:55 +0900

    updated Power.sh, renamed to Battery.sh since it is about battery information

--- config/hypr/scripts/Battery.sh
+++ config/hypr/scripts/Battery.sh
@@ -4,6 +4,6 @@ for i in {0..3}; do
   if [ -f /sys/class/power_supply/BAT$i/capacity ]; then
     battery_level=$(cat /sys/class/power_supply/BAT$i/status)
     battery_capacity=$(cat /sys/class/power_supply/BAT$i/capacity)
-    echo "Power: $battery_capacity% ($battery_level)"
+    echo "Battery: $battery_capacity% ($battery_level)"
   fi
 done
