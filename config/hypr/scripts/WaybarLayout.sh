11236530437df615d3bb3005d0c5ba386af084b9
Jelle Teijema <j.j.teijema@gmail.com>
2025-07-23 23:21:11 +0200

    revert: removal of NOTE

--- config/hypr/scripts/WaybarLayout.sh
+++ config/hypr/scripts/WaybarLayout.sh
@@ -9,6 +9,7 @@ waybar_layouts="$HOME/.config/waybar/configs"
 waybar_config="$HOME/.config/waybar/config"
 SCRIPTSDIR="$HOME/.config/hypr/scripts"
 rofi_config="$HOME/.config/rofi/config-waybar-layout.rasi"
+msg=' 🎌 NOTE: Some waybar LAYOUT NOT fully compatible with some STYLES'
 
 # Apply selected configuration
 apply_config() {
