99c4f84bdce0d81873356ce9b5a2cc13e3866c6a
ahyalfan <alfandi0857@gmail.com>
2025-09-15 13:01:31 +0700

    fix: Update WallustSwww.sh to support binary swww cache (v0.11+)

--- config/hypr/scripts/WallustSwww.sh
+++ config/hypr/scripts/WallustSwww.sh
@@ -19,21 +19,28 @@ cache_file="$cache_dir$current_monitor"
 echo $cache_file
 # Check if the cache file exists for the current monitor output
 if [ -f "$cache_file" ]; then
-    # Get the wallpaper path from the cache file
-    wallpaper_path=$(grep -v 'Lanczos3' "$cache_file" | head -n 1)
-    echo $wallpaper_path
-    # symlink the wallpaper to the location Rofi can access
-    if ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"; then
-        ln_success=true  # Set the flag to true upon successful execution
-    fi
-    # copy the wallpaper for wallpaper effects
-	cp -r "$wallpaper_path" "$HOME/.config/hypr/wallpaper_effects/.wallpaper_current"
+  # Get the wallpaper path from the cache file
+  wallpaper_path=$(grep -v 'Lanczos3' "$cache_file" | head -n 1)
+  # Fallback: If the specified wallpaper path doesn't point to a valid file,
+  # it likely means the cache file is in a binary format. As a workaround,
+  # try extracting readable strings from the cache and use the first relevant line as the wallpaper path.
+  if [[ ! -f "$wallpaper_path" ]]; then
+    echo "[WARN] Binary file format detected, attempting to parse strings..."
+    wallpaper_path=$(strings "$cache_file" | grep -v 'Lanczos3' | head -n 1)
+  fi
+  echo $wallpaper_path
+  # symlink the wallpaper to the location Rofi can access
+  if ln -sf "$wallpaper_path" "$HOME/.config/rofi/.current_wallpaper"; then
+    ln_success=true # Set the flag to true upon successful execution
+  fi
+  # copy the wallpaper for wallpaper effects
+  cp -r "$wallpaper_path" "$HOME/.config/hypr/wallpaper_effects/.wallpaper_current"
 fi
 
 # Check the flag before executing further commands
 if [ "$ln_success" = true ]; then
-    # execute wallust
-	echo 'about to execute wallust'
-    # execute wallust skipping tty and terminal changes
-    wallust run "$wallpaper_path" -s &
+  # execute wallust
+  echo 'about to execute wallust'
+  # execute wallust skipping tty and terminal changes
+  wallust run "$wallpaper_path" -s &
 fi
