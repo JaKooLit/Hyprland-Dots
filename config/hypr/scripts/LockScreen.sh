2c1ce157c97ae690e1dfb8087a3bfa808ac57c18
brockar <martinnguzman.mg@gmail.com>
2025-10-28 18:08:12 -0300

    weatherWrap.sh -> WeatherWrap.sh

--- config/hypr/scripts/LockScreen.sh
+++ config/hypr/scripts/LockScreen.sh
@@ -2,9 +2,10 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 # For Hyprlock
-#pidof hyprlock || hyprlock -q 
+#pidof hyprlock || hyprlock -q
 
 # Ensure weather cache is up-to-date before locking (Waybar/lockscreen readers)
-bash "$HOME/.secret/Hyprland-Dots/config/hypr/UserScripts/weatherWrap.sh" >/dev/null 2>&1
+bash "$HOME/.config/hypr/UserScripts/WeatherWrap.sh" >/dev/null 2>&1
 
 loginctl lock-session
+
