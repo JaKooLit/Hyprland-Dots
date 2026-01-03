ed54ded4d51eb633f7e739467b0ca91a5a28b339
Ahum Maitra <theahummaitra@gmail.com>
2026-01-03 23:05:14 +0530

    enhancement: simplify the program, fix spelling mistakes, add safety mechanism

--- config/hypr/scripts/ThemeChanger.sh
+++ config/hypr/scripts/ThemeChanger.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # SPDX-FileCopyrightText: 2025-present Ahum Maitra theahummaitra@gmail.com
 #
 # SPDX-License-Identifier: 	GPL-3.0-or-later
@@ -7,9 +9,10 @@
 # Repository url : https://github.com/TheAhumMaitra/cautious-waddle
 
 # User choice
-choice=$(wallust theme list |
-  sed '1d; s/^- //' |
-  rofi -dmenu -p "Select Global Theme")
+choice=$(wallust theme list \
+  | sed '1d' \
+  | sed 's/^- //' \
+  | rofi -dmenu -p "Select Global Theme")
 
 # If user requested to exit, then exit
 [[ -z "$choice" ]] && exit 0
@@ -18,7 +21,7 @@ choice=$(wallust theme list |
 wallust theme "$choice"
 
 # Inform user about theme changed
-notify-send "Global theme changed" "Theme selcted $choice"
+notify-send "Global theme changed" "Global Theme selected $choice"
 
 # Give warning to user for Waybar theme refresh
 notify-send "Press SUPER+ALT+R to Refresh Waybar Theme"
