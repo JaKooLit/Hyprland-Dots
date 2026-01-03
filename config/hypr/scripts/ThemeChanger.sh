b57e0457cad3932993b906ae46c7cfa672c10530
Ahum Maitra <theahummaitra@gmail.com>
2026-01-03 18:56:10 +0530

    fix: Add Theme Switcher script

---
+++ config/hypr/scripts/ThemeChanger.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2025-present Ahum Maitra theahummaitra@gmail.com
+#
+# SPDX-License-Identifier: 	GPL-3.0-or-later
+
+# Repository url : https://github.com/TheAhumMaitra/cautious-waddle
+
+# User choice
+choice=$(wallust theme list |
+  sed '1d; s/^- //' |
+  rofi -dmenu -p "Select Global Theme")
+
+# If user requested to exit, then exit
+[[ -z "$choice" ]] && exit 0
+
+# Apply the theme
+wallust theme "$choice"
+
+# Inform user about theme changed
+notify-send "Global theme changed" "Theme selcted $choice"
+
+# Give warning to user for Waybar theme refresh
+notify-send "Press SUPER+ALT+R to Refresh Waybar Theme"
